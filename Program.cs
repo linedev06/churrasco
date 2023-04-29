b2cb12c4ba0ef8119c46fb597f524f22a22b6f13
linedev06 <vitorafelismino@gmail.com>
2023-04-29 14:49:41 -0300

    exercicio feito

--- Program.cs
+++ Program.cs
@@ -3,34 +3,34 @@ int adultoAlcool, adultosemAlcool, adulto, crianca, pessoas;
 double carne, acompanhamentos, cerveja, refrigerante, agua;
 
 
-Console.WriteLine("--- Churrasco ---");
+Console.WriteLine("--- Churrasco ---\n");
 
-Console.Write("Adultos que consomem bebidas alcoólicas: ");
+Console.Write("Adultos que consomem bebidas alcoólicas.......: ");
 adultoAlcool = Convert.ToInt32(Console.ReadLine());
 
 
-Console.Write("Adultos que não consomem bebidas alcoólicas: ");
+Console.Write("Adultos que não consomem bebidas alcoólicas...: ");
 adultosemAlcool = Convert.ToInt32(Console.ReadLine());
 
 
-Console.Write("Crianças: ");
+Console.Write("Quantidade de crianças........................: ");
 crianca = Convert.ToInt32(Console.ReadLine());
 
 adulto = adultoAlcool + adultosemAlcool;
 pessoas = crianca + adulto;
 
-carne = 0.5 * adulto + 0.2 * crianca;
+carne = 0.4 * adulto + 0.2 * crianca;
 acompanhamentos = 0.2 * pessoas;
 cerveja = 2 * adultoAlcool;
-refrigerante = 0.5 * adultosemAlcool + crianca;
+refrigerante = 0.4 * adultosemAlcool + crianca;
 agua = 0.4 * pessoas;
 
 Console.WriteLine();
 
 Console.ForegroundColor = ConsoleColor.Blue;
-Console.WriteLine($"Carne: {carne}kg");
-Console.WriteLine($"Acompanhamentos: {acompanhamentos}kg");
-Console.WriteLine($"Cerveja: {cerveja}L");
-Console.WriteLine($"Refrigerante: {refrigerante}L");
-Console.WriteLine($"Água: {agua}L");
+Console.WriteLine($"Carne.............: {carne:N1} kg");
+Console.WriteLine($"Acompanhamentos...: {acompanhamentos:N1} kg");
+Console.WriteLine($"Cerveja...........: {cerveja:N1} L");
+Console.WriteLine($"Refrigerante......: {refrigerante:N1} L");
+Console.WriteLine($"Água..............: {agua:N1} L");
 Console.ResetColor();
